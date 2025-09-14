3e660a52050b1deee9e2918ec7eaeaccf63fe5bf
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2025-09-14 11:28:27 +0330

    Enhance style

--- components/PublishTime.tsx
+++ components/PublishTime.tsx
@@ -7,13 +7,13 @@ interface PublishTimeProps {
 const PublishTime: FunctionComponent<PublishTimeProps> = ({ publishedTime }) => {
   return (
     <time dateTime={publishedTime} style={{ textTransform: "capitalize" }}>
-      Posted
+      Posted{" "}
       {new Date(publishedTime) > new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
         ? new Intl.RelativeTimeFormat("en-US", { style: "long", numeric: "auto" }).format(
             Math.floor((new Date(publishedTime).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24)),
             "days"
           )
-        : " on " +
+        : "on " +
           new Date(publishedTime).toLocaleDateString("en-US", {
             // year: "numeric",
             month: "long",
