48343b4ae13901e6d6fcbc39fb0eac9b96732627
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2025-09-27 14:44:21 +0330

    test seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { JsonLd, OrgJsonLd } from "@/components/JsonLD";
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -44,22 +42,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(){try{
-	  var ls = localStorage.getItem('theme');
-	  var sys = window.matchMedia('(prefers-color-scheme: dark)').matches;
-	  var dark = ls ? (ls === 'dark') : sys;
-	  var root = document.documentElement.classList;
-	  dark ? root.add('dark') : root.remove('dark');
-	}catch(e){}})();`,
-          }}
-        />
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || ""} />
-        <OrgJsonLd />
-        <JsonLd />
-      </head>
       <body className="font-sans antialiased bg-background-light text-foreground-light dark:bg-background-dark dark:text-foreground-dark transition-colors duration-300">
         <Header />
         <main className="container py-10 min-h-[calc(100dvh-233px)]">{children}</main>
